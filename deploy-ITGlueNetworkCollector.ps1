6006a55ffc4596259a466307262165d12bb925b9
Kai Osthoff <ko@osthoff.net>
2019-07-31 14:35:21 +0200

    Check Log if any errors there, add .net452 install

--- deploy-ITGlueNetworkCollector.ps1
+++ deploy-ITGlueNetworkCollector.ps1
@@ -1,4 +1,7 @@
-﻿# .NET 4.5.2 ist notwendig
+﻿
+
+# Glue Networking need installed .NET 4.5.2
+
 # > offline Installer unter https://support.microsoft.com/de-de/help/2901907/microsoft-net-framework-4-5-2-offline-installer-for-windows-server-201
 # https://kb.itglue.com/hc/en-us/articles/360026301251-Guide-to-successful-Network-Glue-deployment
 
@@ -33,9 +36,114 @@ function Start-MSIInstall {
         $logFile
     )
     Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
+$logfile = "NetworkGlueCollector.msi.log"
+    checkLogfile
+
 
     $location = Get-Location
     Write-Host $location.Path
+}
+
+
+function checkLogfile {
+    param (
+        
+    )
+    write-host $logFile
+    
+    $lines = Get-Content -Path $logFile
+
+    foreach ($line in $lines) {
+        if($line -like "*failed*" -OR $line -like "*error*" -OR $line -like "*requires*") {
+            Write-Host $line
+            if($line -like "*requires .NET Framework 4.5.2*") {
+                Write-Host "Installing .NET452 without any reboot ..."
+                Install-NET452
+                Write-Host "You've to do a reboot on that machine."
+            }
+        }
+    }
+
+}
+
+
+
+function Install-NET452 {
+    Configuration Net452Install
+{
+    node "localhost"
+    {
+ 
+        LocalConfigurationManager
+        {
+            # RebootNodeIfNeeded = $true
+            RebootNodeIfNeeded = $false
+        }
+ 
+        Script Install_Net_4.5.2
+        {
+            SetScript = {
+                $SourceURI = "https://download.microsoft.com/download/B/4/1/B4119C11-0423-477B-80EE-7A474314B347/NDP452-KB2901954-Web.exe"
+                $FileName = $SourceURI.Split('/')[-1]
+                $BinPath = Join-Path $env:SystemRoot -ChildPath "Temp\$FileName"
+ 
+                if (!(Test-Path $BinPath))
+                {
+                    Invoke-Webrequest -Uri $SourceURI -OutFile $BinPath
+                }
+ 
+                write-verbose "Installing .Net 4.5.2 from $BinPath"
+                write-verbose "Executing $binpath /q /norestart"
+                Sleep 5
+                Start-Process -FilePath $BinPath -ArgumentList "/q /norestart" -Wait -NoNewWindow           
+                Sleep 5
+                Write-Verbose "Setting DSCMachineStatus to reboot server after DSC run is completed"
+                $global:DSCMachineStatus = 1
+            }
+ 
+            TestScript = {
+                [int]$NetBuildVersion = 379893
+ 
+                if (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' | %{$_ -match 'Release'})
+                {
+                    [int]$CurrentRelease = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full').Release
+                    if ($CurrentRelease -lt $NetBuildVersion)
+                    {
+                        Write-Verbose "Current .Net build version is less than 4.5.2 ($CurrentRelease)"
+                        return $false
+                    }
+                    else
+                    {
+                        Write-Verbose "Current .Net build version is the same as or higher than 4.5.2 ($CurrentRelease)"
+                        return $true
+                    }
+                }
+                else
+                {
+                    Write-Verbose ".Net build version not recognised"
+                    return $false
+                }
+            }
+ 
+            GetScript = {
+                if (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' | %{$_ -match 'Release'})
+                {
+                    $NetBuildVersion =  (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full').Release
+                    return $NetBuildVersion
+                }
+                else
+                {
+                    Write-Verbose ".Net build version not recognised"
+                    return ".Net 4.5.2 not found"
+                }
+            }
+        }
+    }
+}
+ 
+Net452Install -OutputPath $env:SystemDrive:\DSCconfig
+Set-DscLocalConfigurationManager -ComputerName localhost -Path $env:SystemDrive\DSCconfig -Verbose
+Start-DscConfiguration -ComputerName localhost -Path $env:SystemDrive:\DSCconfig -Verbose -Wait -Force
 }
 
 
@@ -48,6 +156,7 @@ function Start-NetworkGlue {
     )
    Write-Host $COLLECTORArguments
    Start-Process "C:\Program Files (x86)\Network Glue\Collector\bin\register-device.exe" -ArgumentList $COLLECTORArguments
+   
     
 }
 
